c1f08ccb1e50ebe6c331f06b47a4a767d13fca8e
shimizuSarun <sarun@sansenshimizu.com>
2024-10-08 11:28:39 +0900

    complete implement KPI API

--- src/api/kpi/addNewKpiData.ts
+++ src/api/kpi/addNewKpiData.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
 import { ApiResponse, axiosInstance } from 'api'
 
-export interface KpiSaveData {
-  id: number
+export interface AddNewKpiData {
+  year: number
   planSalesRevenue?: number | null
   planVariableCosts?: number | null
   planFixedCosts?: number | null
@@ -11,7 +11,7 @@ export interface KpiSaveData {
   planOrdinaryProfit?: number | null
 }
 
-export default async function saveKpiData(data: KpiSaveData): Promise<ApiResponse<{}>> {
+export default async function addNewKpiData(data: AddNewKpiData): Promise<ApiResponse<{}>> {
   //for beta:test
   // await new Promise(resolve => setTimeout(resolve, 1000))
   // return {
@@ -23,7 +23,7 @@ export default async function saveKpiData(data: KpiSaveData): Promise<ApiRespons
   // }
   // when use real API
   try {
-    const response = await axiosInstance.patch('/api/kpi/' + data.id, data)
+    const response = await axiosInstance.post('/api/kpi', data)
     return { code: 200, message: 'success', data: response.data }
   } catch (error) {
     if (axios.isAxiosError(error) && error.response) {
