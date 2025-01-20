e93565784d899b6e3c1324c4d557ef484c6555b0
shimizuSarun <sarun@sansenshimizu.com>
2025-01-20 15:57:29 +0900

    added function check valid number added text below performace graph

--- src/utils/inputUtils.ts
+++ src/utils/inputUtils.ts
@@ -1,2 +1,6 @@
 export const isShrink = (value: number | undefined | string | null): boolean =>
   !!value || value === 0
+
+export const isValidNumber = (result: number): boolean => {
+  return Number.isFinite(result) && !isNaN(result)
+}
